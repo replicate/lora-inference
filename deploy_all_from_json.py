a05abfa0e4c0e82ceea4a56bbcbbc2b53f403fb6
Simo Ryu <35953539+cloneofsimo@users.noreply.github.com>
2023-03-04 00:46:25 +0900

    Img2img + T2I Adapter + minor bugfixes (#7)
    * ignores
    
    * img2img
    
    * test with img2img args
    
    * test with img2img args
    
    * deploy shell script update
    
    * deploy all
    
    * bug
    
    * t2i adapter
    
    * better args
    
    * deploy
    
    * debugs
    
    * set to none

---
+++ deploy_all_from_json.py
@@ -0,0 +1,34 @@
+import json
+import os
+
+if __name__ == "__main__":
+    with open("./deployments/deploy_infos.json") as f:
+        deploy_infos = json.load(f)
+
+    for model_info in deploy_infos:
+
+        MODEL_ID = model_info["model_id"]
+        SAFETY_MODEL_ID = model_info.get(
+            "safety_model_id", "CompVis/stable-diffusion-safety-checker"
+        )
+        IS_FP16 = model_info.get("is_fp16", 1)
+        USERNAME = "cloneofsimo"  # TODO : this is obviously not the way to do it, but cannot think of a better way right now
+        REPLICATE_MODEL_ID = model_info["name"]
+
+        print(f"Deploying {REPLICATE_MODEL_ID}...")
+        print(f"\tMODEL_ID: {MODEL_ID}")
+        print(f"\tSAFETY_MODEL_ID: {SAFETY_MODEL_ID}")
+        print(f"\tIS_FP16: {IS_FP16}")
+        print(f"\tUSERNAME: {USERNAME}")
+
+        # put this to .env
+        with open(".env", "w") as f:
+            f.write(f"MODEL_ID={MODEL_ID}\n")
+            f.write(f"SAFETY_MODEL_ID={SAFETY_MODEL_ID}\n")
+            f.write(f"IS_FP16={IS_FP16}\n")
+
+        os.system(f"python script/download-weights.py")
+        os.system(
+            f"cog run python test.py --test_img2img --test_text2img --test_adapter"
+        )
+        os.system(f"cog push r8.im/{USERNAME}/{REPLICATE_MODEL_ID}")
--- predict.py
+++ predict.py
@@ -8,6 +8,7 @@ import torch
 from cog import BasePredictor, Input, Path
 from diffusers import (
     StableDiffusionPipeline,
+    StableDiffusionImg2ImgPipeline,
     PNDMScheduler,
     LMSDiscreteScheduler,
     DDIMScheduler,
@@ -15,15 +16,12 @@ from diffusers import (
     EulerAncestralDiscreteScheduler,
     DPMSolverMultistepScheduler,
 )
-from diffusers.pipelines.stable_diffusion.safety_checker import (
-    StableDiffusionSafetyChecker,
-)
-from transformers import CLIPFeatureExtractor
-
+import numpy as np
 
 from lora_diffusion import LoRAManager, monkeypatch_remove_lora
-
-from safetensors.torch import safe_open, save_file
+from t2i_adapters import Adapter
+from t2i_adapters import patch_pipe as patch_pipe_t2i_adapter
+from PIL import Image
 
 import dotenv
 import os
@@ -69,6 +67,29 @@ class Predictor(BasePredictor):
             MODEL_CACHE,
             torch_dtype=torch.float16 if IS_FP16 else torch.float32,
         ).to("cuda")
+
+        patch_pipe_t2i_adapter(self.pipe)
+
+        self.adapters = {
+            ext_type: Adapter.from_pretrained(ext_type).to("cuda")
+            for ext_type, _ in [
+                ("depth", "antique house"),
+                ("seg", "motorcycle"),
+                ("keypose", "elon musk"),
+                ("sketch", "robot owl"),
+            ]
+        }
+
+        self.img2img_pipe = StableDiffusionImg2ImgPipeline(
+            vae=self.pipe.vae,
+            text_encoder=self.pipe.text_encoder,
+            tokenizer=self.pipe.tokenizer,
+            unet=self.pipe.unet,
+            scheduler=self.pipe.scheduler,
+            safety_checker=self.pipe.safety_checker,
+            feature_extractor=self.pipe.feature_extractor,
+        ).to("cuda")
+
         self.token_size_list: list = []
         self.ranklist: list = []
         self.loaded = None
@@ -126,6 +147,14 @@ class Predictor(BasePredictor):
         guidance_scale: float = Input(
             description="Scale for classifier-free guidance", ge=1, le=20, default=7.5
         ),
+        image: Path = Input(
+            description="(Img2Img) Inital image to generate variations of. If this is not none, Img2Img will be invoked.",
+            default=None,
+        ),
+        prompt_strength: float = Input(
+            description="(Img2Img) Prompt strength when providing the image. 1.0 corresponds to full destruction of information in init image",
+            default=0.8,
+        ),
         scheduler: str = Input(
             default="DPMSolverMultistep",
             choices=[
@@ -149,18 +178,31 @@ class Predictor(BasePredictor):
         seed: int = Input(
             description="Random seed. Leave blank to randomize the seed", default=None
         ),
+        adapter_condition_image: Path = Input(
+            description="(T2I-adapter) Adapter Condition Image to gain extra control over generation. If this is not none, T2I adapter will be invoked. Width, Height of this image must match the above parameter, or dimension of the Img2Img image.",
+            default=None,
+        ),
+        adapter_type: str = Input(
+            description="(T2I-adapter) Choose an adapter type for the additional condition.",
+            choices=["sketch", "seg", "keypose", "depth"],
+            default="sketch",
+        ),
     ) -> List[Path]:
         """Run a single prediction on the model"""
         if seed is None:
             seed = int.from_bytes(os.urandom(2), "big")
         print(f"Using seed: {seed}")
 
+        if image is not None:
+            pil_image = Image.open(image).convert("RGB")
+            width, height = pil_image.size
+
+        print(f"Generating image of {width} x {height} with prompt: {prompt}")
+
         if width * height > 786432:
             raise ValueError(
                 "Maximum size is 1024x768 or 768x1024 pixels, because of memory limits. Please select a lower width or height."
             )
-
-        self.pipe.scheduler = make_scheduler(scheduler, self.pipe.scheduler.config)
 
         generator = torch.Generator("cuda").manual_seed(seed)
 
@@ -174,17 +216,81 @@ class Predictor(BasePredictor):
             monkeypatch_remove_lora(self.pipe.unet)
             monkeypatch_remove_lora(self.pipe.text_encoder)
 
-        output = self.pipe(
-            prompt=[prompt] * num_outputs if prompt is not None else None,
-            negative_prompt=[negative_prompt] * num_outputs
-            if negative_prompt is not None
-            else None,
-            width=width,
-            height=height,
-            guidance_scale=guidance_scale,
-            generator=generator,
-            num_inference_steps=num_inference_steps,
-        )
+        # handle t2i adapter
+        w_c, h_c = None, None
+
+        if adapter_condition_image is not None:
+
+            cond_img = Image.open(adapter_condition_image)
+            w_c, h_c = cond_img.size
+
+            if w_c != width or h_c != height:
+                raise ValueError(
+                    "Width and height of the adapter condition image must match the width and height of the generated image."
+                )
+
+            if adapter_type == "sketch":
+                cond_img = cond_img.convert("L")
+                cond_img = np.array(cond_img) / 255.0
+                cond_img = (
+                    torch.from_numpy(cond_img).unsqueeze(0).unsqueeze(0).to("cuda")
+                )
+                cond_img = (cond_img > 0.5).float()
+
+            else:
+                cond_img = cond_img.convert("RGB")
+                cond_img = np.array(cond_img) / 255.0
+
+                cond_img = (
+                    torch.from_numpy(cond_img)
+                    .permute(2, 0, 1)
+                    .unsqueeze(0)
+                    .to("cuda")
+                    .float()
+                )
+
+            with torch.no_grad():
+                adapter_features = self.adapters[adapter_type](cond_img)
+
+            self.pipe.unet.set_adapter_features(adapter_features)
+        else:
+            self.pipe.unet.set_adapter_features(None)
+
+        # either text2img or img2img
+        if image is None:
+            self.pipe.scheduler = make_scheduler(scheduler, self.pipe.scheduler.config)
+
+            output = self.pipe(
+                prompt=[prompt] * num_outputs if prompt is not None else None,
+                negative_prompt=[negative_prompt] * num_outputs
+                if negative_prompt is not None
+                else None,
+                width=width,
+                height=height,
+                guidance_scale=guidance_scale,
+                generator=generator,
+                num_inference_steps=num_inference_steps,
+            )
+        else:
+            extra_kwargs = {
+                "image": pil_image,
+                "strength": prompt_strength,
+            }
+
+            self.img2img_pipe.scheduler = make_scheduler(
+                scheduler, self.pipe.scheduler.config
+            )
+
+            output = self.img2img_pipe(
+                prompt=[prompt] * num_outputs if prompt is not None else None,
+                negative_prompt=[negative_prompt] * num_outputs
+                if negative_prompt is not None
+                else None,
+                guidance_scale=guidance_scale,
+                generator=generator,
+                num_inference_steps=num_inference_steps,
+                **extra_kwargs,
+            )
 
         output_paths = []
         for i, sample in enumerate(output.images):
